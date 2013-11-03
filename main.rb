c575f21c959aefb07f1b923a25276bf293344163
DrewWeth <dgwetherington@gmail.com>
2013-11-03 11:31:53 -0600

    working

--- main.rb
+++ main.rb
@@ -1,15 +1,18 @@
 require 'rubygems'
 require 'oauth'
+require 'json'
+
+
 
 # Change the following values to those provided on dev.twitter.com
 # The consumer key identifies the application making the request.
 # The access token identifies the user making the request.
 consumer_key = OAuth::Consumer.new(
-    "YOUR_CONSUMER_KEY",
-    "YOUR_CONSUMER_SECRET")
+    "laBnLMwlztPiVVMkInoNPQ",
+    "eknfQt4s1oqUToGnvGxUfdeSAm4ELWdqkNLcPQ02jg")
 access_token = OAuth::Token.new(
-    "YOUR_ACCESS_TOKEN",
-    "YOUR_ACCESS_SECRET")
+    "364668892-xtPlJPHcWPSbaSS6AUMo0UkkkHKCA06lK6xuxaxj",
+    "YLcPvyhtdg9S8R9rNKBy7MICQXkVnb1eniPS3OixfZQfr")
 
 # All requests will be sent to this server.
 baseurl = "https://api.twitter.com"
@@ -27,7 +30,34 @@ http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 request = Net::HTTP::Get.new address.request_uri
 request.oauth! http, consumer_key, access_token
 
-# Issue the request and return the response.
+
+# Parse a response from the API and return a user object.
+def parse_user_response(response)
+  user = nil
+
+  # Check for a successful request
+  if response.code == '200'
+    # Parse the response body, which is in JSON format.
+    # ADD CODE TO PARSE THE RESPONSE BODY HERE
+    user = JSON.parse(response.body)
+    screen_name = user
+    # Pretty-print the user object to see what data is available.
+    puts "Hello, #{user["screen_name"]}!"
+  else
+    # There was an error issuing the request.
+    puts "Expected a response of 200 but got #{response.code} instead"
+  end
+
+  user
+end
+
+# Issue the request.
+request = Net::HTTP::Get.new address.request_uri
+request.oauth! http, consumer_key, access_token
 http.start
-response = http.request request
-puts "The response status was #{response.code}"
+response = http.request(request)
+user = parse_user_response(response)
+
+
+
+
