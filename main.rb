2c8cb6296b8992d913a0bebefeaddf4d95b9e257
DrewWeth <dgwetherington@gmail.com>
2013-11-03 12:33:44 -0600

    tweets working

--- main.rb
+++ main.rb
@@ -2,8 +2,6 @@ require 'rubygems'
 require 'oauth'
 require 'json'
 
-
-
 # Change the following values to those provided on dev.twitter.com
 # The consumer key identifies the application making the request.
 # The access token identifies the user making the request.
@@ -14,50 +12,48 @@ access_token = OAuth::Token.new(
     "364668892-xtPlJPHcWPSbaSS6AUMo0UkkkHKCA06lK6xuxaxj",
     "YLcPvyhtdg9S8R9rNKBy7MICQXkVnb1eniPS3OixfZQfr")
 
-# All requests will be sent to this server.
+# Now you will fetch /1.1/statuses/user_timeline.json,
+# returns a list of public Tweets from the specified
+# account.
 baseurl = "https://api.twitter.com"
+path    = "/1.1/statuses/user_timeline.json"
+query   = URI.encode_www_form(
+    "screen_name" => "DrewWeth",
+    "count" => 10,
+)
 
-# The verify credentials endpoint returns a 200 status if
-# the request is signed correctly.
-address = URI("#{baseurl}/1.1/account/verify_credentials.json")
+address = URI("#{baseurl}#{path}?#{query}")
+request = Net::HTTP::Get.new address.request_uri
+
+
+# Parse a response from the API and return a user object.
+def print_tweet(tweets)
+  # ADD CODE TO ITERATE THROUGH EACH TWEET AND PRINT ITS TEXT
+    tweets.each do |tweet|
+
+        puts tweet["text"]
+        puts "\n"  
+    end
+end
+
 
 # Set up Net::HTTP to use SSL, which is required by Twitter.
 http = Net::HTTP.new address.host, address.port
 http.use_ssl = true
 http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 
-# Build the request and authorize it with OAuth.
-request = Net::HTTP::Get.new address.request_uri
-request.oauth! http, consumer_key, access_token
-
-
-# Parse a response from the API and return a user object.
-def parse_user_response(response)
-  user = nil
-
-  # Check for a successful request
-  if response.code == '200'
-    # Parse the response body, which is in JSON format.
-    # ADD CODE TO PARSE THE RESPONSE BODY HERE
-    user = JSON.parse(response.body)
-    screen_name = user
-    # Pretty-print the user object to see what data is available.
-    puts "Hello, #{user["screen_name"]}!"
-  else
-    # There was an error issuing the request.
-    puts "Expected a response of 200 but got #{response.code} instead"
-  end
-
-  user
-end
 
 # Issue the request.
-request = Net::HTTP::Get.new address.request_uri
 request.oauth! http, consumer_key, access_token
 http.start
-response = http.request(request)
-user = parse_user_response(response)
+response = http.request request
 
 
-
-
+# Parse and print the Tweet if the response code was 200
+tweet = nil
+if response.code == '200' then
+  tweet = JSON.parse(response.body)
+  print_tweet(tweet)
+else 
+	puts "getting 404"
+end 
